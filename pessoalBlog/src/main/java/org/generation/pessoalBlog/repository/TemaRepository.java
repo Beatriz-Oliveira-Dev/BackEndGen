015d1943c7707a9287cfc7789d8b06f063dcfaba
Beatriz Oliveira <beatrizlimaoliveira1999@gmail.com>
2021-03-03 22:54:00 -0300

    Subindo teste JUnit

---
+++ pessoalBlog/src/main/java/org/generation/pessoalBlog/repository/TemaRepository.java
@@ -0,0 +1,13 @@
+package org.generation.pessoalBlog.repository;
+
+import java.util.List;
+
+import org.generation.pessoalBlog.model.Tema;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TemaRepository extends JpaRepository<Tema, Long> {
+	public List<Tema> findAllByDescricaoContainingIgnoreCase (String descricao);
+
+}
---
+++ pessoalBlog/src/test/java/org/generation/pessoalBlog/PessoalBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.pessoalBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoalBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
